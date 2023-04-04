3165fab11f83095a6eac371eb6e500f32ce000bd
marypaduano <maria.paduano@gmail.com>
2023-04-04 18:12:25 +0200

    add other routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+// Route::get('/', function () {
+//     return view('contacts');
+// });
+
+// Route::get('/', function () {
+//     return view('about');
+// });
+
+// Route::get('/', function () {
+//     return view('services');
+// });
